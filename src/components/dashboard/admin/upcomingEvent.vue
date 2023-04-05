7f169e522e24d40df46e4872e3c8de9644e57d60
Al-Amin <129585370+alaminbepari@users.noreply.github.com>
2023-04-05 20:41:56 +0600

    update

--- src/components/dashboard/admin/upcomingEvent.vue
+++ src/components/dashboard/admin/upcomingEvent.vue
@@ -1,5 +1,6 @@
-<template>
-  <div class="q-pa-lg">
+<template>div</template>
+<!-- <template> -->
+<!-- <q-card class="q-pa-lg b">
     <div class="row justify-between">
       <div class="">
         <div class="Upcoming">Upcoming Even</div>
@@ -27,25 +28,11 @@
             <div class="calandar-event-name">
               <div>{{ item.title }}</div>
               <div>{{ item.subtitle }}</div>
-=======
-    <div class="row justify-between items-center q-mt-sm">
-      <div class="">
-        <div class="row items-center">
-          <div class="q-mr-sm text-12">08:00 am</div>
-          <div class="calendar-box">
-            <div class="calandar-event-name">
-              <div>Botony</div>
-              <div class="text-12">Lorem ipsum sit amet</div>
->>>>>>> 1040d3227b8e81bd620438b482fb8a6f2491942a
             </div>
           </div>
         </div>
       </div>
-<<<<<<< HEAD
       <div>{{ item.time }}</div>
-=======
-      <div class="text-12">08:00 - 09:00 am</div>
->>>>>>> 1040d3227b8e81bd620438b482fb8a6f2491942a
     </div>
 
     <div class="row justify-between q-mt-lg upcome-event-date">
@@ -73,131 +60,129 @@
       </div>
       <div>{{ item.date }}</div>
     </div>
-  </div>
-</template>
+  </q-card>
+</template> -->
 
-<script setup>
-const event = [
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-    date: "08:00 - 09:00 am",
-  },
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-    date: "08:00 - 09:00 am",
-  },
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-    date: "08:00 - 09:00 am",
-  },
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-    date: "08:00 - 09:00 am",
-  },
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-    date: "08:00 - 09:00 am",
-  },
-];
+<!-- <script setup>
+    const event = [
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+        date: "08:00 - 09:00 am",
+      },
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+        date: "08:00 - 09:00 am",
+      },
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+        date: "08:00 - 09:00 am",
+      },
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+        date: "08:00 - 09:00 am",
+      },
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+        date: "08:00 - 09:00 am",
+      },
+    ];
 
-const events = [
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-  },
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-  },
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-  },
-  {
-    time: "09:00 am",
-    title: "Botony",
-    subtitle: "Lorem ipsum sit amet",
-  },
-];
-</script>
+    const events = [
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+      },
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+      },
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+      },
+      {
+        time: "09:00 am",
+        title: "Botony",
+        subtitle: "Lorem ipsum sit amet",
+      },
+    ];
+  </script>
 
-<style scoped>
-.text-12 {
-  font-size: 12px;
-}
-.Upcoming {
-  font-size: 18px;
-  font-weight: 400;
-}
-.add {
-  background-color: rgba(129, 128, 128, 0.068);
-  width: 20px;
-  height: 20px;
-  color: black;
-  padding: 5px;
-}
-.Month {
-  font-size: 16px;
-  font-weight: 600;
-}
-.calendar-box::before {
-  content: "";
-  width: 4px;
-  height: 85%;
-  position: absolute;
-  border-radius: 25px;
-  background-color: blueviolet;
-}
+  <style scoped>
+    .Upcoming {
+      font-size: 18px;
+      font-weight: 400;
+    }
+    .add {
+      background-color: rgba(129, 128, 128, 0.068);
+      width: 20px;
+      height: 20px;
+      color: black;
+      padding: 5px;
+    }
+    .Month {
+      font-size: 16px;
+      font-weight: 600;
+    }
+    .calendar-box::before {
+      content: "";
+      width: 4px;
+      height: 85%;
+      position: absolute;
+      border-radius: 25px;
+      background-color: blueviolet;
+    }
 
-.calendar-box {
-  display: flex;
-  align-items: center;
-  position: relative;
-}
+    .calendar-box {
+      display: flex;
+      align-items: center;
+      position: relative;
+    }
 
-.calendar-box:before {
-  content: "";
-  position: absolute;
-  background: #3d5ee1;
-  width: 4px;
-  height: 100%;
-  border-radius: 25px;
-}
+    .calendar-box:before {
+      content: "";
+      position: absolute;
+      background: #3d5ee1;
+      width: 4px;
+      height: 100%;
+      border-radius: 25px;
+    }
 
-.calandar-event-name {
-  padding-left: 10px;
-}
+    .calandar-event-name {
+      padding-left: 10px;
+    }
 
-.upcome-event-date:before {
-  content: "";
-  position: absolute;
-  background: #bbb8b8;
-  width: calc(100% - 80px);
-  height: 1px;
-  top: 12px;
-  left: 55px;
-}
+    .upcome-event-date:before {
+      content: "";
+      position: absolute;
+      background: #bbb8b8;
+      width: calc(100% - 80px);
+      height: 1px;
+      top: 12px;
+      left: 55px;
+    }
 
-.upcome-event-date {
-  position: relative;
-  margin: 38px 0px;
-  align-items: center;
-}
+    .upcome-event-date {
+      position: relative;
+      margin: 38px 0px;
+      align-items: center;
+    }
 
-.b {
-  background-color: white;
-}
-</style>
+    .b {
+      background-color: white;
+    }
+  </style>
+</template> -->
