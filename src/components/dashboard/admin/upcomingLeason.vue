9f09ec5f140104b4d3c14b89ce0240d6565cb399
srsohagraj241 <122626431+srsohagraj241@users.noreply.github.com>
2023-04-29 09:50:59 +0600

    New updet

--- src/components/dashboard/admin/upcomingLeason.vue
+++ src/components/dashboard/admin/upcomingLeason.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-card class="q-pa-sm">
-    <div class="flex justify-between">
+  <q-card class="q-pa-md">
+    <div class="flex justify-between q-mb-md">
       <div class="text-h6">Upcoming Leason</div>
       <div class="text-blue-8">View all courses</div>
     </div>
@@ -16,13 +16,29 @@
       <div>
         <div class="text-right">Confirmed</div>
         <div class="q-mt-md">
-          <q-btn color="blue-8" label="Resheduled" size="sm"></q-btn>
+          <q-btn color="blue-8" label="Resheduled" size="md"></q-btn>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-between q-mt-md">
+      <div>
+        <div>Leason 30</div>
+        <div>3.1 lorem</div>
+        <div>
+          <q-icon name="date_range" /> Sep 5, 2022 |
+          <q-icon name="schedule" /> 09:00 - 10:00 am
+        </div>
+      </div>
+      <div>
+        <div class="text-right">Confirmed</div>
+        <div class="q-mt-md">
+          <q-btn color="blue-8" label="Resheduled" size="md"></q-btn>
         </div>
       </div>
     </div>
   </q-card>
 </template>
-    
+
 <script setup>
 // const upcomingLeasonList = [
 //     {
