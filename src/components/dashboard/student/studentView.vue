4e7dfd472b99352eca47a6757697a8fe383c2f39
srsohagraj241 <122626431+srsohagraj241@users.noreply.github.com>
2023-05-19 16:07:51 +0600

    update

--- src/components/dashboard/student/studentView.vue
+++ src/components/dashboard/student/studentView.vue
@@ -1,59 +1,10 @@
 <template>
-  <q-page>
-    <div>
-      <div class="row justify-between items-center">
-        <div class="text-h6 text-bold">Students Details</div>
-        <div class="text-body1">
-          <div class="row">
-            <div class="text-bold cursor-pointer">Student</div>
-            <span class=""> / </span>
-            <div class="">Student Details</div>
-          </div>
-        </div>
+  <q-page class="q-pa-lg">
+    <div class="row justify-between">
+      <div class="text-h6">Teachers Details</div>
+      <div class="text-body1">
+        <span>Teachers </span>/<span>Teachers Details</span>
       </div>
-    </div>
-    <div class="q-pa-md">
-      <q-card>
-        <q-page>
-          <div class="">
-            <div class="flex justify-between items-center">
-              <div class="text-h5 text-weight-medium q-pa-md">Profile</div>
-              <div class="text-h5 text-weight-medium q-pa-md">
-                <q-icon name=" more_vert" color="grey-7"></q-icon>
-              </div>
-            </div>
-            <div class="q-pa-md">
-              <div class="">
-                <img
-                  src="https://preschool.dreamguystech.com/template/assets/img/profile-bg.jpg"
-                  alt="dddd"
-                />
-              </div>
-              <div class="flex q-col-gutter-xl q-pa-md">
-                <div>
-                  <div class="text-bold text-body1">Bruce Willis</div>
-                  <div class="text-grey-7">Computer Science</div>
-                </div>
-                <div>
-                  <div class="">Followers</div>
-                  <div class="text-bold text-body1">2850</div>
-                </div>
-                <div>
-                  <div class="">Followers</div>
-                  <div class="text-bold text-body1">2850</div>
-                </div>
-                <div>
-                  <div class="">Followers</div>
-                  <div class="text-bold text-body1">2850</div>
-                </div>
-                <div>
-                  <q-btn rounded color="primary" label=" Rounded" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </q-page>
-      </q-card>
     </div>
   </q-page>
 </template>
