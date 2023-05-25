9088c61aeee91a30b78bf42a4f2b1fb2826fb559
srsohagraj241 <122626431+srsohagraj241@users.noreply.github.com>
2023-05-26 00:58:13 +0600

    update student view

--- src/components/dashboard/student/studentView.vue
+++ src/components/dashboard/student/studentView.vue
@@ -1,10 +1,281 @@
 <template>
-  <q-page class="q-pa-lg">
+  <q-page class="bg-grey-2 q-pa-lg">
     <div class="row justify-between">
       <div class="text-h6">Teachers Details</div>
       <div class="text-body1">
         <span>Teachers </span>/<span>Teachers Details</span>
       </div>
     </div>
+
+    <q-card class="card-1 q-pa-md q-mt-lg">
+      <div class="row q-pa-sm justify-between">
+        <div class="text-h5 text-weight-medium">Profile</div>
+        <div class="text-h5">
+          <q-icon name="more_vert" color="grey-7"></q-icon>
+        </div>
+      </div>
+
+      <div>
+        <div class="q-pa-sm">
+          <q-img
+            class="img"
+            src="https://preschool.dreamguystech.com/template/assets/img/profile-bg.jpg"
+          ></q-img>
+        </div>
+
+        <div class="row items-center justify-between">
+          <div class="">
+            <q-img
+              class="profile"
+              src="https://preschool.dreamguystech.com/template/assets/img/profile-user.jpg"
+            ></q-img>
+          </div>
+
+          <div class="row items-center">
+            <div class="icon">
+              <q-btn class="btn" round>
+                <q-icon name="drive_file_rename_outline"></q-icon>
+              </q-btn>
+            </div>
+          </div>
+
+          <div class="Profilename q-pa-md">
+            <div class="text-body1 text-weight-medium">Bruce Willis</div>
+            <div>Computer Science</div>
+          </div>
+        </div>
+
+        <div class="q-ml-lg">
+          <div
+            class="text flex q-gutter-lg q-pa-md items-center justify-between full-width"
+          >
+            <div class="flex q-gutter-md">
+              <div class="">
+                <div>Followers</div>
+                <div class="text-body1 text-bold">2850</div>
+              </div>
+              <div>
+                <div>Followers</div>
+                <div class="text-body1 text-bold">2850</div>
+              </div>
+              <div>
+                <div>Followers</div>
+                <div class="text-body1 text-bold">2850</div>
+              </div>
+            </div>
+
+            <div class="flex q-gutter-md">
+              <div class="button text-white">
+                <q-btn push color="indigo-14" label="Follow" />
+              </div>
+              <div class="button text-white">
+                <q-btn push color="teal-4" label="Message" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </q-card>
+
+    <div class="row">
+      <q-card class="card-2 q-pa-md q-mt-lg">
+        <div class="q-pa-md">
+          <div class="">
+            <div class="text-h6">Personal Details:</div>
+
+            <div class="row q-mt-xs q-gutter-lg items-center">
+              <q-icon class="text-h6" name="person" />
+              <div>
+                <div class="size text-weight-medium">Name</div>
+                <div>Bruce Willis</div>
+              </div>
+            </div>
+            <div class="row q-mt-xs q-gutter-lg items-center">
+              <q-icon class="text-h6" name="apartment" />
+              <div>
+                <div class="size text-weight-medium">departmet</div>
+                <div>Computer Scirnce</div>
+              </div>
+            </div>
+            <div class="row q-mt-xs q-gutter-lg items-center">
+              <q-icon class="text-h6" name="wifi_calling_3" />
+              <div>
+                <div class="size text-weight-medium">Mobile</div>
+                <div>01307203678</div>
+              </div>
+            </div>
+            <div class="row q-mt-xs q-gutter-lg items-center">
+              <q-icon class="text-h6" name="email" />
+              <div>
+                <div class="size text-weight-medium">email</div>
+                <div>srsohagraj241@gmail.com</div>
+              </div>
+            </div>
+            <div class="row q-mt-xs q-gutter-lg items-center">
+              <q-icon class="text-h6" name="person_outline" />
+              <div>
+                <div class="size text-weight-medium">gender</div>
+                <div>Male</div>
+              </div>
+            </div>
+            <div class="row q-mt-xs q-gutter-lg items-center">
+              <q-icon class="text-h6" name="calendar_month" />
+              <div>
+                <div class="size text-weight-medium">date of Birth</div>
+                <div>02-03-2000</div>
+              </div>
+            </div>
+            <div class="row q-mt-xs q-gutter-lg items-center">
+              <q-icon class="text-h6" name="language" />
+              <div>
+                <div class="size text-weight-medium">language</div>
+                <div>Bangla, English, Hindi</div>
+              </div>
+            </div>
+            <div class="row q-mt-xs q-gutter-lg items-center">
+              <q-icon class="text-h6" name="location_on" />
+              <div>
+                <div class="size text-weight-medium">Address</div>
+                <div>Bara Muraripur</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </q-card>
+
+      <div class="q-pa-md">
+        <q-card class="card-3 q-pa-md q-mt-sm">
+          <div class="q-pa-md">
+            <div>
+              <div class="text-h6">About Me</div>
+            </div>
+            <div>
+              <div class="size-1 q-mt-lg text-weight-medium">
+                Hello I am Daisy Parks
+              </div>
+            </div>
+            <div>
+              <div class="q-mt-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur officia deserunt mollit anim id est
+                laborum.
+              </div>
+            </div>
+            <div>
+              <div class="q-mt-lg">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam inventore veritatis et
+                quasi architecto beatae vitae dicta sunt explicabo.
+              </div>
+            </div>
+            <div>
+              <div class="size-1 q-mt-lg text-weight-medium">Education</div>
+            </div>
+            <div>
+              <div class="q-mt-lg">2008 - 2009</div>
+              <div class="q-mt-xs">
+                Secondary Schooling at xyz school of secondary education,
+                Mumbai.
+              </div>
+            </div>
+            <div class="q-mt-lg">
+              <div>2008 - 2009</div>
+              <div class="q-mt-xs">
+                Secondary Schooling at xyz school of secondary education,
+                Mumbai.
+              </div>
+            </div>
+            <div>
+              <div class="q-mt-lg">2008 - 2009</div>
+              <div class="q-mt-xs">
+                Secondary Schooling at xyz school of secondary education,
+                Mumbai.
+              </div>
+            </div>
+            <div class="q-mt-lg">
+              <div>2008 - 2009</div>
+              <div class="q-mt-xs">
+                Secondary Schooling at xyz school of secondary education,
+                Mumbai.
+              </div>
+            </div>
+          </div>
+        </q-card>
+      </div>
+    </div>
+
+    <div>
+      <q-card class="q-pa-md q-mt-lg">
+        <div>
+          <div>Skils:</div>
+        </div>
+        <div>
+          <div>
+            <q-linear-progress
+              rounded
+              size="20px"
+              color="warning"
+              class="q-mt-sm"
+            />
+          </div>
+        </div>
+      </q-card>
+    </div>
   </q-page>
 </template>
+
+<style scoped>
+.card-1 {
+  border-radius: 10px;
+}
+.img {
+  border-radius: 10px;
+}
+
+.profile {
+  width: 120px;
+  margin-top: -90px;
+  margin-left: 50px;
+  border-radius: 50%;
+  border: 4px solid;
+  border-color: rgb(240, 235, 235);
+}
+
+.icon {
+  color: rgb(255, 255, 255);
+  margin-left: -80px;
+  margin-top: -30px;
+  background-color: rgb(76, 0, 255);
+  border-radius: 50px;
+  cursor: pointer;
+  overflow: hidden;
+}
+.btn {
+  background-color: rgb(76, 0, 255);
+}
+
+.Profilename {
+  margin-left: 30px;
+}
+.card-2 {
+  border-radius: 10px;
+  width: 500px;
+}
+
+.size {
+  font-size: 18px;
+}
+
+.card-3 {
+  border-radius: 10px;
+  width: 720px;
+  margin-right: -100px;
+}
+.size-1 {
+  font-size: 18px;
+}
+</style>
