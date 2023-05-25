bdf7aacdedfb82d1096065664f5c45e38bd80ee3
Al-Amin <129585370+alaminbepari@users.noreply.github.com>
2023-05-25 14:32:43 +0600

    update

--- src/components/invoicedasboard/addInvoice.vue
+++ src/components/invoicedasboard/addInvoice.vue
@@ -12,6 +12,83 @@
         <div><q-btn color="blue" label="Save Draft" /></div>
       </div>
     </div>
+
+    <q-card class="q-mt-lg q-pa-md">
+      <div class="row q-col-gutter-md">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-12">
+          <div>
+            <div>
+              <div>Customer Name</div>
+              <div class="q-mt-md">
+                <q-select outlined label="Standard" />
+              </div>
+            </div>
+            <div class="q-mt-md">
+              <div>Phone Number</div>
+              <div class="q-mt-md">
+                <q-select outlined label="Standard" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-6 col-sm-12 col-12">
+          <div>
+            <div>
+              <div>Customer Name</div>
+              <div class="q-mt-md">
+                <q-select outlined label="Standard" />
+              </div>
+            </div>
+            <div class="q-mt-md">
+              <div>Phone Number</div>
+              <div class="q-mt-md">
+                <q-select outlined label="Standard" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 col-12">
+          <div>
+            <div>
+              <div>Customer Name</div>
+              <div class="q-mt-md">
+                <q-select outlined label="Standard" />
+              </div>
+            </div>
+            <div class="q-mt-md">
+              <div>Phone Number</div>
+              <div class="q-mt-md">
+                <q-select outlined label="Standard" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <q-separator inset class="q-mt-xl" />
+      <div class="q-mt-lg row q-col-gutter-md">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-12">
+          <div class="row justify-between">
+            <div>Invoice From</div>
+            <div>Edit Address</div>
+          </div>
+          <div>
+            <div>Darren Elder</div>
+            <div>806 Twin Willow Lane, Old Forge,</div>
+            <div>Newyork, USA</div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+          <div>
+            <div>Invoice From</div>
+          </div>
+          <div>
+            <div>Darren Elder</div>
+            <div>806 Twin Willow Lane, Old Forge,</div>
+            <div>Newyork, USA</div>
+          </div>
+        </div>
+      </div>
+    </q-card>
   </q-page>
 </template>
 <style scoped>
