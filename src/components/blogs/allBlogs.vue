24c8998da573766f40661595f2ed3ae61c70c2e2
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-06-18 22:55:02 +0600

    upd

--- src/components/blogs/allBlogs.vue
+++ src/components/blogs/allBlogs.vue
@@ -7,8 +7,13 @@
             padding="10px"
             label="Active Blogs"
             outline
-            color="red"
-            text-color="blue"
+            :class="[!activeBlogsModel ? 'bg-white' : 'bg-grey-4']"
+            :text-color="!activeBlogsModel ? 'blue' : 'black'"
+            @click="
+              () => {
+                activeBlogsModel = false;
+              }
+            "
           />
         </div>
         <div>
@@ -16,9 +21,14 @@
             padding="10px"
             label="Panding Blogs"
             outline
-            color="red"
-            text-color="blue"
+            :class="[activeBlogsModel ? 'bg-white' : 'bg-grey-4']"
+            :text-color="activeBlogsModel ? 'blue' : 'black'"
             percentage="0.0<*< 100.0"
+            @click="
+              () => {
+                activeBlogsModel = true;
+              }
+            "
           />
         </div>
       </div>
@@ -35,7 +45,7 @@
     <div class="q-mt-lg row q-col-gutter-md">
       <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-12">
         <q-card class="q-pa-md">
-          <div>
+          <div style="position: relative">
             <div style="position: relative">
               <q-img
                 src="https://preschool.dreamguystech.com/template/assets/img/category/blog-6.jpg"
@@ -49,8 +59,8 @@
                 height: 30px;
                 border-radius: 10px;
                 position: absolute;
-                top: 120px;
-                left: 35px;
+                top: 10px;
+                left: 10px;
               "
             >
               <q-icon name="visibility" />
@@ -79,31 +89,31 @@
 
           <div class="row justify-between items-center q-mt-sm">
             <div class="flex items-center">
+              <q-btn
+                size="md"
+                style="height: 35px; border-radius: 8px"
+                icon-left
+              >
+                <q-icon name="border_color" style="font-size: 14px" />
+                <span class="q-ml-sm">Edit</span>
+              </q-btn>
               <div
-                style="
-                  border-radius: 7px;
-                  background-color: rgba(230, 32, 32, 0.807);
-                  color: rgb(255, 250, 250);
-                  padding: 6px;
-                  width: 70px;
-                "
-                class="cursor-pointer"
+                style="border-radius: 7px; padding: 6px; width: 70px"
+                class="cursor-pointer bg-green-2 text-green-10"
               >
                 <q-icon class="q-mr-sm" name="border_color" />Edit
               </div>
               <div
-                style="
-                  border-radius: 7px;
-                  color: rgb(255, 250, 250);
-                  padding: 6px;
-                  width: 80px;
-                "
-                class="cursor-pointer bg-green q-ml-sm q-pa-xs"
+                style="border-radius: 7px; padding: 6px; width: 80px"
+                class="cursor-pointer bg-red-2 text-red-10 q-ml-sm q-pa-xs"
               >
                 <q-icon class="q-mr-sm" name="delete" />Delete
               </div>
             </div>
-            <div class="cursor-pointer text-red">
+            <div v-if="activeBlogsModel" class="cursor-pointer text-red">
+              <q-toggle v-model="card1Toggler" color="green" />
+            </div>
+            <div v-else class="cursor-pointer text-red">
               <q-icon name="visibility_off" /> Inactive
             </div>
           </div>
@@ -125,8 +135,8 @@
                 height: 30px;
                 border-radius: 10px;
                 position: absolute;
-                top: 120px;
-                left: 35px;
+                top: 10px;
+                left: 10px;
               "
             >
               <q-icon name="visibility" />
@@ -500,4 +510,6 @@
 import { ref } from "vue";
 
 const current = ref(3);
+const activeBlogsModel = ref(false);
+const card1Toggler = ref(true);
 </script>
